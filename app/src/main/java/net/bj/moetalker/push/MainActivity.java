c2aadca0c79d8c040262d56d4bf3f0caa5f5c980
BJ4501 <neko.bj4501@gmail.com>
2017-12-01 15:54:40 +0800

    activity绘制，相关styles.xml添加

--- app/src/main/java/net/bj/moetalker/push/MainActivity.java
+++ app/src/main/java/net/bj/moetalker/push/MainActivity.java
@@ -1,20 +1,38 @@
 package net.bj.moetalker.push;
 
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
 
 import net.bj.moetalker.common.Common;
 import net.bj.moetalker.common.app.Activity;
+import net.bj.moetalker.common.widget.PortraitView;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 
 public class MainActivity extends Activity  {
 
-
+    @BindView(R.id.appbar)
+    View mLayAppbar;
+    @BindView(R.id.im_portrait)
+    PortraitView mPortrait;
+    @BindView(R.id.txt_title)
+    TextView mTitle;
+    @BindView(R.id.lay_container)
+    FrameLayout mContainer;
+    @BindView(R.id.navigation)
+    BottomNavigationView mNavigation;
 
     @Override
     protected int getContentLayoutId() {
@@ -22,8 +40,33 @@ public class MainActivity extends Activity  {
     }
 
     @Override
+    protected void initWidget() {
+        super.initWidget();
+        Glide.with(this).load(R.drawable.bg_src_morning).centerCrop().into(new ViewTarget<View,GlideDrawable>(mLayAppbar) {
+            //当资源准备好的时候，将图片设置为背景
+            @Override
+            public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                //这里的this是ViewTarget，不是MainActivity
+                this.view.setBackground(resource.getCurrent());
+            }
+        });
+
+    }
+
+    @Override
     protected void initData() {
         super.initData();
     }
+
+    @OnClick(R.id.im_search)
+    void onSearchMenuClick(){
+
+    }
+
+    @OnClick(R.id.btn_action)
+    void onActionClick(){
+
+    }
+
 
 }
