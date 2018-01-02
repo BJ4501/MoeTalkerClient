16cfe2e47cd2328ae2aff2fafbbdb675553cbfc7
BJ4501 <neko.bj4501@gmail.com>
2018-01-02 18:48:13 +0800

    对RecyclerView进行一个简单的Presenter封装

---
+++ factory/src/main/java/net/bj/talker/factory/data/user/ContractDataSource.java
@@ -0,0 +1,9 @@
+package net.bj.talker.factory.data.user;
+
+/**
+ * 联系人数据源
+ * Created by Neko-T4 on 2018/1/2.
+ */
+
+public class ContractDataSource {
+}
---
+++ factory/src/main/java/net/bj/talker/factory/data/user/ContractRepository.java
@@ -0,0 +1,9 @@
+package net.bj.talker.factory.data.user;
+
+/**
+ * 联系人仓库
+ * Created by Neko-T4 on 2018/1/2.
+ */
+
+public class ContractRepository {
+}
